f7a537a4472ab3e2ac1e8c42419cad9d9356cbeb
noCH3COOH <13178669726@163.com>
2024-04-26 22:02:16 +0800

    20240426@2201
    done：程序完成，测试成功

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ import math
 # import cv2 as cv
 import huffman as hf
 
-log = open("log.md", "w+")
-
-def make_log(str):
+def make_log(log, str):
     log.write(str + '\n')
-    print(str)
+    # print(str)
 
-if __name__ == "__main__":
-    path_src = "src.bmp"
-    path_dst = "src.bmp.hf"
+def process(path_src, path_dst, path_log):
+
+    log = open(path_log, "w+", encoding="UTF-8")
+
     hashmap_src = [0 for i in range(0, 256)]
 
     # ==================== 码元统计 ====================
@@ -38,47 +37,47 @@ if __name__ == "__main__":
 
     output, output_symbol, encode_output, encode_output_len = hf.encode(P_src_symbol, 2)    # 2元编码
 
-    make_log("【编码报告】")
-
-    if 1 != sum(output):
-        make_log("结果出错：" + str(sum(output)))
+    make_log(log, "【编码报告】")
     
-    # 计算平均码长
+    # 计算平均码长、平均信息熵、编码效率
     average_len = 0
     H_U = 0
     for p, lenght in zip(output, encode_output_len):
         average_len += p*lenght
         H_U += (- (p*math.log2(p)) )
     
-    make_log("平均码长：" + str(average_len))
-    make_log("编码效率：" + str(H_U / (average_len * math.log2(2)) * 100) + "%")
+    make_log(log, "平均码长：" + str(average_len))
+    make_log(log, "平均信息熵：" + str(H_U))
+    make_log(log, "编码效率：" + str( (H_U / average_len) * 100 ) + "%")
     
     # 判断是否唯一可译码
     flag = 0
     for i in range(len(encode_output)):
         for j in range(i+1, len(encode_output)):
             if encode_output[j][0:len(encode_output[i])] == encode_output[i]:
-                make_log("非唯一可译码")
+                make_log(log, "非唯一可译码")
                 flag = 1
                 break
     
     if 0 == flag:
-        make_log("唯一可译码")
+        make_log(log, "唯一可译码：是")
+    else:
+        make_log(log, "唯一可译码：否")
     
     # 计算编码的方差
     sigma_I = 0
     for p in output:
-        sigma_I += p*(((-math.log2(p)) - H_U) ** 2 )
+        sigma_I += p*(((-math.log2(p)) - H_U) * ((-math.log2(p)) - H_U) )
     
-    make_log("方差：" + str(sigma_I))
+    make_log(log, "方差：" + str(sigma_I))
     
     # 输出
-    # output = [round(r, 3) for r in output]
+    output = [round(r, 3) for r in output]
     
-    make_log('|次数|概率|码元|霍夫曼编码|码长|')
-    make_log('|:-:|:-:|:-:|:-:|:-:|')
+    make_log(log, '|次数|概率|码元|霍夫曼编码|码长|')
+    make_log(log, '|:-:|:-:|:-:|:-:|:-:|')
     for times_sym, p, symbol, encode_r, encode_r_len in zip(hashmap_src, output, [("$P_{" + symbol + "}$") for symbol in output_symbol], encode_output, encode_output_len):
-        make_log("|" + str(times_sym) + "|" + str(p) + "|" + str(symbol) + "|" + str(encode_r) + "|" + str(encode_r_len) + "|")
+        make_log(log, "|" + str(times_sym) + "|" + str(p) + "|" + str(symbol) + "|" + str(encode_r) + "|" + str(encode_r_len) + "|")
 
     # ==================== 文件写入 ====================
 
@@ -95,9 +94,10 @@ if __name__ == "__main__":
             # 将字节转换为无符号整数
             readByte = ord(readByte)
 
-            cache_dst += encode_output[readByte]
+            cache_dst += encode_output[readByte]    # 写入编码
 
             if 0 == (len(cache_dst) % 8):    # 凑够了
+                
                 while "" != cache_dst:
                     byte_toWrite = 0
 
@@ -109,8 +109,21 @@ if __name__ == "__main__":
 
                     dst.write(byte_toWrite.to_bytes(1, "big"))    # 写入最前面的一个字节
                     cache_dst = cache_dst[8:]    # 去掉已写入的
+                
+                cache_dst = ""    # 清空缓冲区
+        
             else:
-                cache_dst += encode_output[readByte]    # 凑不够，丢到缓冲区后面
+                continue    # 凑不够
+                
     
     src.close()
     dst.close()
+
+if __name__ == "__main__":
+    path_src_1 = "1.bmp"
+    path_src_2 = "2.bmp"
+    path_src_3 = "3.bmp"
+
+    process(path_src_1, (path_src_1 + ".hf"), (path_src_1 + ".md"))
+    process(path_src_2, (path_src_2 + ".hf"), (path_src_2 + ".md"))
+    process(path_src_3, (path_src_3 + ".hf"), (path_src_3 + ".md"))
